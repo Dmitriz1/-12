0b0b91d636bcd0315c2fdb700c82a7dd408ea3bb
SergeyLyubimov <lyubimovvsergey@gmail.com>
2026-05-07 21:27:11 +1000

    Добавлена заглушка на auth_required

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
+from app.models.user import User
 from app.schemas.user import UserRegister, UserLogin
 from app.services.auth_service import register_user, login_user
 
 router = APIRouter(prefix="/auth")
 
+# TODO: ЗАГЛУШКА! Изменить когда начнем работать с фронт-эндом
+def auth_required(db: Session = Depends(get_db)):
+    user_id = db.query(User).first().id
+    return user_id
 
 @router.post("/register")
 def register(data: UserRegister, db: Session = Depends(get_db)):
